a898463f2699512065366141fdc65ba8ebfc7b15
darmie <dreakinlaja@gmail.com>
2026-02-05 09:37:27 +0000

    fix: allow needless_return in test_llvm_simple

--- compiler/examples/test_llvm_simple.rs
+++ compiler/examples/test_llvm_simple.rs
@@ -1,4 +1,4 @@
-#![allow(unused_imports, unused_variables, dead_code)]
+#![allow(unused_imports, unused_variables, dead_code, clippy::needless_return)]
 //! Debug LLVM JIT with arrays
 
 #[cfg(feature = "llvm-backend")]
